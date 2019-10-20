da226f08ff6b0072f94c0a8dd2d33cf3d7f94cea
I_Am_Stephan <stephanburger54@gmail.com>
2019-10-20 20:02:04 +0200

    added boxicons to the project

--- src/globalComponents/topbar/topbar.js
+++ src/globalComponents/topbar/topbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import styles from './topbar.module.scss';
 
+import Icon from '../../components/icon/icon';
+
 export default class Topbar extends Component {
   render() {
     return (
       <nav className={`${styles['topbar']}`}>
-
+        <Icon name='rocket' />
       </nav>
     );
   }
