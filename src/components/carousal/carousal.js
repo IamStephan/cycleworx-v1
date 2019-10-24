c487b858d39b4e278dbb12edc92dccf735002226
I_Am_Stephan <stephanburger54@gmail.com>
2019-10-24 19:30:44 +0200

    added an image carousal component, put it in the home screen and tweaked the home screen to be black again

---
+++ src/components/carousal/carousal.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import globals from '../../utils/global.scss';
+import styles from './carousal.module.scss';
+
+import Icon from '../icon/icon';
+
+export default class Carousal extends Component {
+  static propTypes = {
+    images: PropTypes.arrayOf(PropTypes.shape({
+      src: PropTypes.node,
+      title: PropTypes.string
+    }))
+  }
+
+  static defaultProps = {
+    images: []
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      activeIndex: 0
+    }
+
+    this.slideRight = this.slideRight.bind(this)
+    this.slideLeft = this.slideLeft.bind(this)
+
+    this.Interval = setInterval(() => {
+      this.slideRight()
+    }, 5000)
+  }
+
+  slideRight() {
+    if(this.state.activeIndex < this.props.images.length - 1) {
+      this.setState((prevState, prevProps) => ({
+        ...prevState,
+        activeIndex: prevState.activeIndex + 1
+      }))
+    } else {
+      this.setState((prevState, prevProps) => ({
+        ...prevState,
+        activeIndex: 0
+      }))
+    }
+  }
+
+  slideLeft() {
+    if(this.state.activeIndex > 0) {
+      this.setState((prevState, prevProps) => ({
+        ...prevState,
+        activeIndex: prevState.activeIndex - 1
+      }))
+    } else {
+      this.setState((prevState, prevProps) => ({
+        ...prevState,
+        activeIndex: this.props.images.length - 1
+      }))
+    }
+  }
+
+  render() {
+    return (
+      <div className={`${styles['carousal']}`}>
+        
+        {
+          this.props.images.map((item, i) => (
+            <div
+              key={i}
+              className={`${styles['image-container']} ${this.state.activeIndex === i ? styles['active'] : ''}`}
+            >
+              <div className={`${styles['elements']}`}>
+                <div className={`${styles['controls']}`}>
+                  <div onClick={() => {
+                    this.slideLeft()
+                    clearInterval(this.Interval)
+                  }} className={`${styles['left-btn']}`}>
+                    <Icon name='left-arrow' type='solid' color={globals.light} />
+                  </div>
+
+                  <div onClick={() => {
+                    this.slideRight()
+                    clearInterval(this.Interval)
+                  }} className={`${styles['right-btn']}`}>
+                    <Icon name='right-arrow' type='solid' color={globals.light} />
+                  </div>
+                </div>
+
+                <div className={`${styles['overlay']}`}>
+                  <h1 className={`${styles['title']}`}>
+                    {item.title}
+                  </h1>
+                </div>
+              </div>
+              <img
+                className={`${styles['image']}`}
+                src={item.src}
+                {...this.props}
+              />
+            </div>
+          ))
+        }
+
+      </div>
+    );
+  }
+}
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
+import globals from '../../utils/global.scss';
 import styles from './home.module.scss'
+
+import test from '../../static/riley-harrison-9TNGeodpVEA-unsplash.jpg';
+import test2 from '../../static/mark-northern-qvk8QFyGfWA-unsplash.jpg';
+
+import Carousal from '../../components/carousal/carousal'
 
 export default class Home extends Component {
   render() {
     return (
       <div className={`${styles['home']}`}>
-        
+        <div className={`${styles['carousal-space']}`}>
+          <Carousal
+            images={[
+              {src: test, title: 'Welcome to cycleworx langebaan'},
+              {src: test2, title: 'test2'},
+            ]}
+          />
+        </div>
+
+        <section>
+          <h1 style={{
+            color: globals.light,
+            textAlign: 'center',
+            padding: 50
+          }}>
+            Our Services
+          </h1>
+        </section>
       </div>
-    );
+    )
   }
 }
