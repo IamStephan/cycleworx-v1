6e559096b7960dd13415eaa0ec545db22b45b851
I_Am_Stephan <stephanburger54@gmail.com>
2019-10-27 13:22:14 +0200

    added contact page, routes load dynamicaly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'mobx-react';
+import Loadable from 'react-loadable';
 
 import globals from './utils/global.scss';
 
 import Topbar from './globalComponents/topbar/topbar'
 import Footer from './globalComponents/footer/footer'
 
-import Home from './pages/home/home';
-
 import AppStore from './stores/appStore'
+
+// Loadable components
+const Home = Loadable({
+  loader: () => import('./pages/home/home'),
+  loading: () => (<div>Loading...</div>)
+})
+
+const Contact = Loadable({
+  loader: () => import('./pages/contact/contact'),
+  loading: () => (<div>Loading...</div>)
+})
 
 const stores = {
   AppStore
